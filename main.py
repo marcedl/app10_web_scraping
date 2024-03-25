10ae2fa117696e66ef40ab962d3a9036a5ad74cf
Marcela DL <leon.ari00@hotmail.com>
2024-03-25 11:53:00 +0100

    Data stored in a txt file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 import selectorlib
 
+data = r"C:\Users\leona\OneDrive\Escritorio\Python Mega Course Build 20 Apps\app10_web_scraping\data.txt"
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def scrape(url):
     #Scrape the page source from the URL
@@ -12,12 +14,33 @@ def scrape(url):
     source = response.text
     return source
 
+
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(source)["tours"]
     return value
 
+
+def send_email():
+    print("Email was sent")
+
+
+def store(extracted):
+    with open(data, "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open(data, "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
